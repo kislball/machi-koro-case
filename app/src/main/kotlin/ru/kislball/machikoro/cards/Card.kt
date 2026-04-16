230911d94d68eb7e75e143bebeea9cef346796f9
Georgiy Mikhailenko <kislball@yahoo.com>
2026-04-16 18:16:36 +0300

    Add CardKind to Card

--- app/src/main/kotlin/ru/kislball/machikoro/cards/Card.kt
+++ app/src/main/kotlin/ru/kislball/machikoro/cards/Card.kt
@@ -6,6 +6,7 @@ import ru.kislball.machikoro.game.Triggerable
 abstract class Card : Triggerable() {
     abstract val cardNameKey: String
     abstract val cardNameDescription: String
+    abstract val kind: CardKind
 
     abstract fun getPrice(s: Step): Int
 }
