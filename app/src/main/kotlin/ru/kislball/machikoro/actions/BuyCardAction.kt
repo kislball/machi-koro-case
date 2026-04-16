284aca7ffde584bef3240f94dc9356ff65a36bf1
Georgiy Mikhailenko <kislball@yahoo.com>
2026-04-16 18:18:02 +0300

    Implement BuyCardAction

---
+++ app/src/main/kotlin/ru/kislball/machikoro/actions/BuyCardAction.kt
@@ -0,0 +1,27 @@
+package ru.kislball.machikoro.actions
+
+import ru.kislball.machikoro.cards.CardKind
+import ru.kislball.machikoro.effects.CompoundEffect
+import ru.kislball.machikoro.effects.Effect
+import ru.kislball.machikoro.effects.GrantCardEffect
+import ru.kislball.machikoro.effects.MoneyTransferEffect
+import ru.kislball.machikoro.game.DiceRolledStep
+import ru.kislball.machikoro.game.Player
+import ru.kislball.machikoro.game.Step
+
+class BuyCardAction(player: Player, var kind: CardKind) : PlayerAction(player) {
+    override fun checkValid(s: Step) {
+        require(player.balance >= kind.basePrice) { "Player doesn't have enough balance" }
+        require(s.game.countCardsOfKind(kind) < kind.totalCards) { "No more cards available" }
+        require(s is DiceRolledStep) { "Buying cards is only available during buying stage" }
+        require(player == s.currentPlayer) { "Only current player can buy cards" }
+    }
+
+    override fun getEffect(s: Step): Effect {
+        checkValid(s)
+        return CompoundEffect.combineEffects(
+            MoneyTransferEffect(player, null, kind.basePrice),
+            GrantCardEffect(player, kind),
+        )
+    }
+}
