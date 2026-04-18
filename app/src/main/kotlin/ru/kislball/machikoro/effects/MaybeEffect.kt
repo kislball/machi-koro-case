9305696618ae066279527713d310408404832ebe
Georgiy Mikhailenko <kislball@yahoo.com>
2026-04-18 14:03:36 +0300

    Implement MaybeEffect

---
+++ app/src/main/kotlin/ru/kislball/machikoro/effects/MaybeEffect.kt
@@ -0,0 +1,16 @@
+package ru.kislball.machikoro.effects
+
+import ru.kislball.machikoro.game.Step
+
+class MaybeEffect(val inner: Effect) : Effect() {
+    override var effectDescriptionKey = inner.effectDescriptionKey
+    override var effectNameKey = inner.effectNameKey
+
+    override fun apply(step: Step) {
+        try {
+            inner.apply(step)
+        } catch (e: Exception) {
+            print("MaybeEffect: effect ${inner.javaClass.simpleName} emitted error ${e.message}")
+        }
+    }
+}
