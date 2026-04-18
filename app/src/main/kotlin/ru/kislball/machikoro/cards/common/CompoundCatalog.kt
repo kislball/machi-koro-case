cf7ce232e3a9db0bc15ce2848b454c19e8cfec34
Georgiy Mikhailenko <kislball@yahoo.com>
2026-04-18 16:27:20 +0300

    Fix CompoundCatalog

--- app/src/main/kotlin/ru/kislball/machikoro/cards/common/CompoundCatalog.kt
+++ app/src/main/kotlin/ru/kislball/machikoro/cards/common/CompoundCatalog.kt
@@ -1,3 +1,3 @@
 package ru.kislball.machikoro.cards.common
 
-class CompoundCatalog(cards: List<List<Card>>) : CardCatalog(cards.flatten()) {}
+class CompoundCatalog(cards: List<List<Card>>) : CardCatalog(*cards.flatten().toTypedArray())
