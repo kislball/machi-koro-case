bc7389496be31c5462021345ebba49981829d75a
Georgiy Mikhailenko <kislball@yahoo.com>
2026-04-19 15:08:01 +0300

    Add remove card effect

---
+++ app/src/main/kotlin/ru/kislball/machikoro/effects/cards/RemoveCardEffect.kt
@@ -0,0 +1,20 @@
+package ru.kislball.machikoro.effects.cards
+
+import ru.kislball.machikoro.cards.common.Card
+import ru.kislball.machikoro.effects.Effect
+import ru.kislball.machikoro.game.Player
+import ru.kislball.machikoro.game.step.StepPhase
+
+class RemoveCardEffect(
+    val from: Player,
+    val card: Card,
+) : Effect("effects.remove_card") {
+    override fun isValid(stepPhase: StepPhase): Boolean {
+        require(from.cards.contains(card)) { "Player does not have the card to remove" }
+        return true
+    }
+
+    override fun run(stepPhase: StepPhase) {
+        from.cards.remove(card)
+    }
+}
