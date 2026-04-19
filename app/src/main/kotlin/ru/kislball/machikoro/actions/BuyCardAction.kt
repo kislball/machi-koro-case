c243c9ba9518dcca591b9b32cb6a5c25c5a1ef40
Georgiy Mikhailenko <kislball@yahoo.com>
2026-04-19 15:07:05 +0300

    Bring back buying stage condition

--- app/src/main/kotlin/ru/kislball/machikoro/actions/BuyCardAction.kt
+++ app/src/main/kotlin/ru/kislball/machikoro/actions/BuyCardAction.kt
@@ -5,8 +5,10 @@ import ru.kislball.machikoro.effects.cards.GrantCardEffect
 import ru.kislball.machikoro.effects.money.MoneyTransferEffect
 import ru.kislball.machikoro.effects.money.MoneyTransferType
 import ru.kislball.machikoro.effects.utility.CompoundEffect
+import ru.kislball.machikoro.game.DiceRollResult
 import ru.kislball.machikoro.game.Game
 import ru.kislball.machikoro.game.Player
+import ru.kislball.machikoro.game.contains
 import ru.kislball.machikoro.game.step.StepPhase
 
 class BuyCardAction(game: Game, player: Player, id: String) :
@@ -19,6 +21,7 @@ class BuyCardAction(game: Game, player: Player, id: String) :
 
   override fun checkValid(s: StepPhase) {
     require(player.balance >= card.getPrice(s)) { "Player doesn't have enough balance" }
+      require(s.results.contains<DiceRollResult>()) { "Buying is only available after dice have been rolled" }
     require(s.game.countCardsOfKind(card) < card.totalCards) { "No more cards available" }
     require(player == s.currentPlayer) { "Only current player can buy cards" }
   }
