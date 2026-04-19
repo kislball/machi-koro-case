3e459e0f29720ab412dbc976cc22e760f1a42090
Georgiy Mikhailenko <kislball@yahoo.com>
2026-04-19 12:36:27 +0300

    Add isValid check to CompoundEffect

--- app/src/main/kotlin/ru/kislball/machikoro/effects/utility/CompoundEffect.kt
+++ app/src/main/kotlin/ru/kislball/machikoro/effects/utility/CompoundEffect.kt
@@ -4,6 +4,10 @@ import ru.kislball.machikoro.effects.Effect
 import ru.kislball.machikoro.game.step.StepPhase
 
 class CompoundEffect(val effects: List<Effect>) : Effect("effects.compound") {
+  override fun isValid(stepPhase: StepPhase): Boolean {
+    return effects.all { it.isValid(stepPhase) }
+  }
+
   override fun run(stepPhase: StepPhase) {
     for (effect in effects) {
       effect.apply(stepPhase)
