a2b69dd82a5d508f2a9ea1f7e856364b166df788
Georgiy Mikhailenko <kislball@yahoo.com>
2026-04-25 00:14:55 +0300

    Add some basic game stage ui

--- core/src/main/kotlin/ru/kislball/machikoro/facility/GameDriver.kt
+++ core/src/main/kotlin/ru/kislball/machikoro/facility/GameDriver.kt
@@ -31,7 +31,6 @@ import ru.kislball.machikoro.game.DiceRollResult
 import ru.kislball.machikoro.game.Game
 import ru.kislball.machikoro.game.IntermediateRollResult
 import ru.kislball.machikoro.game.Player
-import ru.kislball.machikoro.game.markers.canThrowTwoDice
 import ru.kislball.machikoro.game.step.FinishedStepPhase
 import ru.kislball.machikoro.game.step.PendingStepPhase
 import ru.kislball.machikoro.game.utilities.contains
