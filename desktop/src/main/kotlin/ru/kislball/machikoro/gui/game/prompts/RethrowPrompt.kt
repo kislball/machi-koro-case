a42e372f8e3449d5f024046320a6eee762c28074
Georgiy Mikhailenko <kislball@yahoo.com>
2026-04-28 17:29:14 +0300

    Change rethrow prompt layout

--- desktop/src/main/kotlin/ru/kislball/machikoro/gui/game/prompts/RethrowPrompt.kt
+++ desktop/src/main/kotlin/ru/kislball/machikoro/gui/game/prompts/RethrowPrompt.kt
@@ -2,6 +2,7 @@ package ru.kislball.machikoro.gui.game.prompts
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
@@ -16,8 +17,8 @@ fun RethrowPrompt(
     onSelect: (Boolean) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-  Card(modifier = modifier.width(920.dp)) {
-    Column {
+  Card(modifier = modifier.width(340.dp)) {
+    Column(modifier = Modifier.padding(10.dp)) {
       Text("$playerName, хотите перебросить кости?")
       Row {
         Button(onClick = { onSelect(true) }) { Text("Перебросить") }
