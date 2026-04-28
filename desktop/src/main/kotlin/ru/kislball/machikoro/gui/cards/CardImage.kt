e694ffc0f94d7d041680bd67c725db0f17b4ddef
Georgiy Mikhailenko <kislball@yahoo.com>
2026-04-28 13:48:44 +0300

    Make cards smaller

--- desktop/src/main/kotlin/ru/kislball/machikoro/gui/cards/CardImage.kt
+++ desktop/src/main/kotlin/ru/kislball/machikoro/gui/cards/CardImage.kt
@@ -9,8 +9,8 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import ru.kislball.machikoro.cards.common.Card
 
-val CardImageWidth = 160.dp
-val CardImageHeight = 240.dp
+val CardImageWidth = 144.dp
+val CardImageHeight = 216.dp
 
 fun Card.imageResourcePath(): String {
   return "cards/${cardId.removePrefix("cards.")}.png"
